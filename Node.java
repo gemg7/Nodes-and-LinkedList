a5e5ebe3216a291f2ff7384ad2c1a413bc4d2788
gemg7 <46541430+gemg7@users.noreply.github.com>
2019-02-25 14:52:35 -0800

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,36 +1,20 @@
 public class Node<E> {
-   private E data;
-   private Node<E> next;
+   public E data;
+   public Node<E> next;
    
-   public Node(E data) {
+   Node(E data) {
       this(data, null);
    }
    
-   public Node(E data, Node<E> next) {
+   Node(E data, Node<E> next) {
       this.data = data;
       this.next = next;
    }
    
-   public boolean equals(Node<E> node) {
-      if(this.data == node) {
+   boolean equals(Node<E> node) {
+      if(this.data == node.data) {
          return true;
       } 
       return false;
-   }
-}
-
-class LinkedList<E> {
-   private Node<E> first;
-   
-   public void add(Node<E> node) {
-      if(this.first == null) {
-         this.first = node;
-      } else {
-         Node current = this.first;
-         while (current.next != null) {
-            current = current.next;
-         }
-         current.next = node;
-      }
    }
 }
